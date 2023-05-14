64a0811c7fc512e2190a81c405fbd4b3a22972cc
Cleiver <creibis10@gmail.com>
2023-05-14 18:44:41 -0300

    feat atualizar customer por id

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -63,3 +63,46 @@ export async function createCustomer(req, res) {
     res.status(500).send(err.message);
   }
 }
+
+export async function updateCustomer(req, res) {
+    const id = Number(req.params.id);
+    const { name, phone, cpf, birthday} = req.body;
+
+    if (isNaN(id)) return res.sendStatus(400);
+  
+    const useSchemaCadastro = joi.object({
+      name: joi.string().required(),
+      phone: joi.string().required(),
+      cpf: joi.string().required(),
+      birthday: joi.string().pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/, {
+          name: "date yyyy/mm/dd",
+      }).required()
+    })
+  
+    if(isNaN(cpf) || cpf.split('').length !== 11 || (phone.split('').length !== 11 && phone.split('').length !== 10)) return res.status(400).send("valores invalidos")
+  
+    const validacao = useSchemaCadastro.validate(req.body, {abortEarly: false})
+    if (validacao.error) {
+        const errors = validacao.error.details.map((detail) => detail.message);
+      return res.status(400).send(errors);
+    }
+
+    
+  
+    try {
+      // Implemente essa função também
+    
+      const cpfJaCadastrado = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf]);
+      console.log((cpfJaCadastrado.rows[0].id === id))
+      console.log(id)
+      if(cpfJaCadastrado.rows[0].id !== id) return res.status(409).send("id de outro usuario ja cadastrado");
+
+
+      await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`,
+          [name, phone, cpf, birthday, id]);
+      res.sendStatus(200);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+  
--- src/routers/customers.router.js
+++ src/routers/customers.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createCustomer, findAllCustomers, findCustomersById } from "../controllers/customers.controller.js";
+import { createCustomer, findAllCustomers, findCustomersById, updateCustomer } from "../controllers/customers.controller.js";
 
 const customerRouter = Router();
 
 customerRouter.get("/customers", findAllCustomers);
 customerRouter.get("/customers/:id", findCustomersById);
 customerRouter.post("/customers", createCustomer);
+customerRouter.put("/customers/:id", updateCustomer);
 
 export default customerRouter;
